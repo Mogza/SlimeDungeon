4a7f5c57a4f685225499564ac8141b60b509281d
Maël-Mamadou Diagne <mael-mamadou.diagne@epitech.eu>
2023-02-18 10:13:40 +0100

    (+) Add : Max Level (10) and possibility to split your maxlevel slime

---
+++ SlimeSplit.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity ^0.8.13;
+import "./SlimeGame.sol";
+import "./SlimeXp.sol";
+import "./SlimeDungeon.sol";
+
+contract SlimeSplit is SlimeDungeon {
+
+    function splitting(Slime memory slime, string memory new_name) public {
+        require(slime.level >= 10);
+        slime.level = slime.level / 2;
+        SlimesList.push(Slime(new_name, slime.level, uint32(block.timestamp + cooldownValue)));
+        ownerOfSlime[new_name] = msg.sender;
+        slimeCount[msg.sender] += 1;
+    }
+}
--- SlimeXp.sol
+++ SlimeXp.sol
@@ -7,20 +7,24 @@ import "./SlimeGame.sol";
 contract SlimeXp is SlimeGame{
     
     uint neededXpBottles = 1;
+    uint LvlUpFee = 0.001 ether;
 
     function Give1Xp(Slime memory targetSlime) public view {
         require(msg.sender == ownerOfSlime[targetSlime.name], "This is not your slime.");
+        require(targetSlime.level < 10, "Your slime is max level (try to split him ^^).");
         require(OneXpBottles[msg.sender] >= neededXpBottles, "You need an xp bottle to do this.");
         targetSlime.level += 1;
     }
 
     function Give2Xp(Slime memory targetSlime) public view {
         require(msg.sender == ownerOfSlime[targetSlime.name], "This is not your slime.");
+        require(targetSlime.level < 9, "Please use a '1 xp' to reach the max level of your slime.");
         require(TwoXpBottles[msg.sender] >= neededXpBottles, "You need an xp bottle to do this.");
         targetSlime.level += 2;
     }
 
     function Pay1Xp(Slime memory slime) external payable {
+        require(slime.level < 10, "Your slime is max level (try to split him ^^).");
         require(msg.value == LvlUpFee);
         slime.level += 1;
     }
