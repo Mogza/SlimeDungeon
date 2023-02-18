4d03375408df2c5c38d300ee7dfb53ddbb52b7b4
Maël-Mamadou Diagne <mael-mamadou.diagne@epitech.eu>
2023-02-18 10:15:26 +0100

    (~) Fix : level of splitted slimes

--- SlimeSplit.sol
+++ SlimeSplit.sol
@@ -9,8 +9,8 @@ import "./SlimeDungeon.sol";
 contract SlimeSplit is SlimeDungeon {
 
     function splitting(Slime memory slime, string memory new_name) public {
-        require(slime.level >= 10);
-        slime.level = slime.level / 2;
+        require(slime.level == 10);
+        slime.level = 2;
         SlimesList.push(Slime(new_name, slime.level, uint32(block.timestamp + cooldownValue)));
         ownerOfSlime[new_name] = msg.sender;
         slimeCount[msg.sender] += 1;
